eb8541bcc63de4303490a258daeaac793b837809
miltondw <estradamilton2001@gmail.com>
2022-12-06 10:29:04 -0500

    fusion mobil-menu

--- js/script.js
+++ js/script.js
@@ -1,8 +1,17 @@
 const EmailMenu = document.getElementById("email-menu");
 const DeskMenu = document.getElementById("desk-menu");
+const MobilMenu = document.getElementById("mobil-menu");
 const IconArrow = document.getElementById("icon-arrow");
+const IconBurger = document.getElementById("icon-burger");
+const iconClose = document.querySelector(".icon-close");
 
 EmailMenu.addEventListener("click", () => {
   DeskMenu.classList.toggle("translate");
   IconArrow.classList.toggle("rotate");
+});
+IconBurger.addEventListener("click", () => {
+  MobilMenu.classList.toggle("translate");
+});
+iconClose.addEventListener("click", () => {
+  MobilMenu.classList.remove("translate");
 });

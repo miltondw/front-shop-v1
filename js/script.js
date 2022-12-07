a85382fdebdee4c74c49bdf7e812c9d1a1e0a3b0
miltondw <estradamilton2001@gmail.com>
2022-12-07 14:59:05 -0500

    card products with js

--- js/script.js
+++ js/script.js
@@ -36,3 +36,81 @@ NavbarIconCart.addEventListener("click", () => {
   }
   ShoppingCart.classList.toggle("translate");
 });
+
+const CardProducts = document.getElementById("card-products");
+const ProductList = [
+  {
+    image:
+      "https://cdn.pixabay.com/photo/2020/10/21/18/07/laptop-5673901_960_720.jpg",
+    price: 3500000,
+    productName: "Laptop",
+  },
+  {
+    image:
+      "https://cdn.pixabay.com/photo/2020/10/21/18/07/laptop-5673901_960_720.jpg",
+    price: 3500000,
+    productName: "Laptop",
+  },
+  {
+    image:
+      "https://cdn.pixabay.com/photo/2020/10/21/18/07/laptop-5673901_960_720.jpg",
+    price: 3500000,
+    productName: "Laptop",
+  },
+  {
+    image:
+      "https://cdn.pixabay.com/photo/2020/10/21/18/07/laptop-5673901_960_720.jpg",
+    price: 3500000,
+    productName: "Laptop",
+  },
+  {
+    image:
+      "https://cdn.pixabay.com/photo/2020/10/21/18/07/laptop-5673901_960_720.jpg",
+    price: 3500000,
+    productName: "Laptop",
+  },
+  {
+    image:
+      "https://cdn.pixabay.com/photo/2020/10/21/18/07/laptop-5673901_960_720.jpg",
+    price: 3500000,
+    productName: "Laptop",
+  },
+];
+function currencyFormat(num) {
+  return "$" + num.toFixed(0).replace(/(\d)(?=(\d{3})+(?!\d))/g, "$1,");
+}
+
+function renderProduct(arr) {
+  for (const product of arr) {
+    const productCard = document.createElement("div");
+    productCard.classList.add("card-product");
+
+    const img = document.createElement("img");
+    img.classList.add("product-img");
+    img.setAttribute("src", product.image);
+    img.setAttribute("alt", product.productName);
+
+    const cardContent = document.createElement("div");
+    cardContent.classList.add("card-subtitle_content");
+
+    const cardSubtitle = document.createElement("p");
+    cardSubtitle.classList.add("card-subtitle");
+    cardSubtitle.innerHTML = ` <span class="prices">${currencyFormat(
+      product.price
+    )}</span>
+    ${product.productName}`;
+
+    const iconCartContent = document.createElement("figure");
+    iconCartContent.classList.add("icon_cart");
+
+    const iconCart = document.createElement("img");
+    iconCart.setAttribute("src", "./images/icons/bt_add_to_cart.svg");
+    iconCart.setAttribute("alt", "icon carShop");
+
+    productCard.append(img, cardContent);
+    cardContent.append(cardSubtitle, iconCartContent);
+    iconCartContent.appendChild(iconCart);
+    CardProducts.appendChild(productCard);
+  }
+}
+renderProduct(ProductList);
